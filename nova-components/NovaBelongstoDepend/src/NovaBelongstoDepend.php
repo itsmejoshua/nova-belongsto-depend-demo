8192c9d380dfdf1007edde88087e34da995b3002
Sergej Tihonov <sergej@visifo.com>
2021-02-21 16:18:11 +0100

    created empty nova field orlyapps/nova-belongsto-depend package.

---
+++ nova-components/NovaBelongstoDepend/src/NovaBelongstoDepend.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Orlyapps\NovaBelongstoDepend;
+
+use Laravel\Nova\Fields\Field;
+
+class NovaBelongstoDepend extends Field
+{
+    /**
+     * The field's component.
+     *
+     * @var string
+     */
+    public $component = 'nova-belongsto-depend';
+}
